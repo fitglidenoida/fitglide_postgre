797607e599e8f0adbba1b56c5686e6552ef01542
fitglidenoida <info@fitglide.in>
2025-04-03 13:45:00 +0530

    add index to strapi users permission

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,18 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {
+    strapi.service('plugin::users-permissions.user').update = async (id: string, data: any) => {
+      const sanitizedData = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        mobile: data.mobile,
+        email: data.email // Allow email update if needed
+      };
+      const updatedUser = await strapi.entityService.update('plugin::users-permissions.user', id, {
+        data: sanitizedData
+      });
+      return updatedUser;
+    };
+  },
 };
