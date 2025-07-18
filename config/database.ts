a9d40dc93f6b00e3cbecd2a22babef60c32f08d0
TrailBlaze Wellness <info@fitglide.in>
2025-07-18 19:31:52 +0530

    update utc+5:30

--- config/database.ts
+++ config/database.ts
@@ -11,6 +11,8 @@ export default ({ env }) => {
         user: env('DATABASE_USERNAME', 'fitglide_dbadmin'),
         password: env('DATABASE_PASSWORD', 'S@ndysango1982'),
         ssl: env.bool('DATABASE_SSL', false),
+        timezone: 'Asia/Kolkata', // or '+05:30'
+
       },
       pool: { min: 2, max: 10 },
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
